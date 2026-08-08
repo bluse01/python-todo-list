6ddfe84f8f3b8de20f74242214f764f6802b4236
bluse01 <zukaguruli07@gmail.com>
2026-08-08 20:46:29 +0400

    feat: added new cli parser and removed user input in the main.py

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
+import argparse
 from manager.create import main as create
 from manager.delete import main as delete
 
-print("todo app - python")
-print("1. create new todo")
-print("2. delete a task")
+parser = argparse.ArgumentParser(prog="task-tracker-cli")
 
-# using recursively func to get the valid user input
-def get_user_input():
-  try:
-    return int(input("input: "))
-  except ValueError:
-    print("invalid input try again!")
-    return get_user_input()
+subparsers = parser.add_subparsers(dest="command", required=True)
 
-user_input = get_user_input()
-match user_input:
-  case 1:
+parser_add = subparsers.add_parser("add", help="create new todo")
+parser_add.add_argument("description", help="todos description")
+parser_add.add_argument("--status", choices=["todo", "in-progress", "done"], default="in-progress", help="todos status")
+
+parser_delete = subparsers.add_parser("delete", help="deletes the todo by id")
+parser_delete.add_argument("id", type=int, help="the id of the todo")
+
+args = parser.parse_args()
+
+match args.command:
+  case "add":
     create()
-  case 2:
+  case "delete":
     delete()
