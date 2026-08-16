f4a54a297d7ff23d8720f628ffcdaa2be4839577
bluse01 <zukaguruli07@gmail.com>
2026-08-16 21:40:22 +0400

    refactored list.py

--- manager/list.py
+++ manager/list.py
@@ -1,5 +1,6 @@
-import json
 from datetime import datetime
+
+from manager.todo_manager import TaskManager
 
 
 def format_iso_to_local(iso_string: str) -> str:
@@ -12,35 +13,27 @@ def format_iso_to_local(iso_string: str) -> str:
 	return dt_local.strftime("%b %d, %Y at %I:%M %p")
 
 
-def read_config(target: str, limit: int):
-	try:
-		with open("config.json", "r") as config:
-			config_parsed = json.load(config)
+def read_config(config: dict, filter_target: str, limit: int):
+	if filter_target:
+		filtered_list = {
+			k: v for k, v in config.items() if v["status"] == filter_target
+		}
+	else:
+		filtered_list = dict(config)
 
-		if target:
-			filtered_list = {
-				k: v for k, v in config_parsed.items() if v["status"] == target
-			}
-		else:
-			filtered_list = dict(config_parsed)
+	for i, (k, v) in enumerate(filtered_list.items()):
+		if i > limit - 1:
+			break
 
-		for i, (k, v) in enumerate(filtered_list.items()):
-			if i > limit - 1:
-				break
+		formatted_created = format_iso_to_local(v["createdAt"])
+		formatted_updated = format_iso_to_local(v["updatedAt"])
 
-			formatted_created = format_iso_to_local(v["createdAt"])
-			formatted_updated = format_iso_to_local(v["updatedAt"])
-
-			print(
-				f"id: {k} - description: {v['desc']} | status: {v['status']} | createdAt: {formatted_created} | updatedAt: {formatted_updated}"
-			)
-
-	except FileNotFoundError:
-		print("Config file doesn't exist yet Create a todo first!")
-	except json.JSONDecodeError:
-		print("Config file is empty or corrupted")
-		print("Try deleting config.json or writing new todo if the json is empty")
+		print(
+			f"id: {k} - description: {v['desc']} | status: {v['status']} | createdAt: {formatted_created} | updatedAt: {formatted_updated}"
+		)
 
 
 def main(user_input):
-	read_config(user_input.status, user_input.limit)
+	manager = TaskManager()
+	config = manager.load()
+	read_config(config, user_input.status, user_input.limit)
